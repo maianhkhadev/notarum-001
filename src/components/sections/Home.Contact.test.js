fcfcadfbc25667960d3f6d47cb0133ed0d4cb4fb
Mai Anh Kha <maianhkha.dev@gmail.com>
2019-02-02 15:55:31 +0700

    update footer

--- src/components/sections/Home.Contact.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Section from './Home.Contact';
-
-it('renders correctly', () => {
-  const tree = renderer.create(<Section />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
