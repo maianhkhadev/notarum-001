c6e70bf7f77c9b679f9b6bee0d664a7e29c8531e
Mai Anh Kha <maianhkha.dev@gmail.com>
2019-02-11 10:19:19 +0700

    optimize image

--- src/components/layouts/Default.presentation.js
+++ src/components/layouts/Default.presentation.js
@@ -22,104 +22,44 @@ export default function (props) {
 
       <footer className="layout-footer">
         <div className="container">
-          <section className="section-01">
-            <div className="row">
-              <div className="col-xl-3">
-                <h5 className="footer-title">Products</h5>
+          <div className="row">
+            <div className="col-xl-3">
+              <h5 className="footer-title">Quicklinks</h5>
 
-                <ul className="footer-nav">
-                  <li className="footer-item">
-                    <a className="footer-link" href="index.html">Lorem Ipsum</a>
-                  </li>
-                  <li className="footer-item">
-                    <a className="footer-link" href="index.html">Lorem Ipsum</a>
-                  </li>
-                  <li className="footer-item">
-                    <a className="footer-link" href="index.html">Lorem Ipsum</a>
-                  </li>
-                </ul>
-              </div>
-              <div className="col-xl-3">
-                <h5 className="footer-title">Resources</h5>
-
-                <ul className="footer-nav">
-                  <li className="footer-item">
-                    <a className="footer-link" href="index.html">Docs</a>
-                  </li>
-                  <li className="footer-item">
-                    <a className="footer-link" href="index.html">Guides</a>
-                  </li>
-                  <li className="footer-item">
-                    <a className="footer-link" href="index.html">Release Notes</a>
-                  </li>
-                  <li className="footer-item">
-                    <a className="footer-link" href="index.html">Blogs</a>
-                  </li>
-                </ul>
-              </div>
-              <div className="col-xl-3">
-                <h5 className="footer-title">Company</h5>
-
-                <ul className="footer-nav">
-                  <li className="footer-item">
-                    <a className="footer-link" href="index.html">About Us</a>
-                  </li>
-                  <li className="footer-item">
-                    <a className="footer-link" href="index.html">Contact Us</a>
-                  </li>
-                  <li className="footer-item">
-                    <a className="footer-link" href="index.html">Career</a>
-                  </li>
-                  <li className="footer-item">
-                    <a className="footer-link" href="index.html">Customers</a>
-                  </li>
-                </ul>
-              </div>
-              <div className="col-xl-3">
-                <h5 className="footer-title">Quicklinks</h5>
-
-                <ul className="footer-nav">
-                  <li className="footer-item">
-                    <a className="footer-link" href="index.html">Support</a>
-                  </li>
-                  <li className="footer-item">
-                    <a className="footer-link" href="index.html">Terms</a>
-                  </li>
-                  <li className="footer-item">
-                    <a className="footer-link" href="index.html">Privacy Policy</a>
-                  </li>
-                </ul>
-              </div>
+              <ul className="footer-nav">
+                <li className="footer-item">
+                  <a className="footer-link" href="index.html">Support</a>
+                </li>
+                <li className="footer-item">
+                  <a className="footer-link" href="index.html">Terms</a>
+                </li>
+                <li className="footer-item">
+                  <a className="footer-link" href="index.html">Privacy Policy</a>
+                </li>
+              </ul>
             </div>
-          </section>
+            <div className="col-xl-3">
+              <h5 className="footer-title">Get Social</h5>
 
-          <section className="section-02">
-            <div className="row">
-              <div className="col-xl-3">
-                <h5 className="footer-title">Get Social</h5>
-
-                <ul className="footer-nav">
-                  <li className="footer-item">
-                    <a className="footer-link" href="index.html">Facebook</a>
-                  </li>
-                  <li className="footer-item">
-                    <a className="footer-link" href="index.html">Twitter</a>
-                  </li>
-                  <li className="footer-item">
-                    <a className="footer-link" href="index.html">Linkedin</a>
-                  </li>
-                </ul>
-              </div>
-              <div className="col-xl-3">
-                <h5 className="footer-title">Singapore</h5>
-                <div className="address">32 Carpenter Street 059911 Singapore</div>
-              </div>
-              <div className="col-xl-3">
-              </div>
-              <div className="col-xl-3">
-              </div>
+              <ul className="footer-nav">
+                <li className="footer-item">
+                  <a className="footer-link" href="index.html">Facebook</a>
+                </li>
+                <li className="footer-item">
+                  <a className="footer-link" href="index.html">Twitter</a>
+                </li>
+                <li className="footer-item">
+                  <a className="footer-link" href="index.html">Linkedin</a>
+                </li>
+              </ul>
             </div>
-          </section>
+            <div className="col-xl-3">
+              <h5 className="footer-title">Singapore</h5>
+              <div className="address">32 Carpenter Street 059911 Singapore</div>
+            </div>
+            <div className="col-xl-3">
+            </div>
+          </div>
         </div>
 
         <div className="copyright">
